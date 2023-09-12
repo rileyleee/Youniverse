36726795b58a7a934b574392c4f78e0816dc8d0c
박영기 <rl0428@naver.com>
2023-09-12 16:55:33 +0900

    초기 설정

---
+++ Back_End/src/main/java/com/ssafy/youniverse/YouniverseApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.youniverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouniverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouniverseApplication.class, args);
+    }
+
+}
