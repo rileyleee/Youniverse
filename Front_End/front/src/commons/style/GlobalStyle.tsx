09653c6eaf91a83e93e94a268f0209436bacd195
이은경 <whddlto2782@gmail.com>
2023-09-18 20:28:57 +0900

    S09P22B204-149 Feat: 버튼 컴포넌트 생성

--- Front_End/front/src/commons/style/GlobalStyle.tsx
+++ Front_End/front/src/commons/style/GlobalStyle.tsx
@@ -1,1 +1,7 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        padding: 0;
+    }
+`;
