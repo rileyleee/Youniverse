50e74379e99a10b48a3c7535a8bf1fddd1a63cd2
선다영 <seondy99@gmail.com>
2023-09-19 23:22:59 +0900

    S09P22B204-148 Feat: SharedStyle, LayoutStyle 생성

--- Front_End/front/src/App.tsx
+++ Front_End/front/src/App.tsx
@@ -11,123 +11,115 @@ import Text from "./components/atoms/Text";
 import IconBox from "./components/atoms/IconBox";
 import { LOGO, MAIN_NOT_LOGIN } from "./commons/constants/String";
 import { GlobalStyles } from "./commons/style/GlobalStyle";
-
 const sayHelloHandler = () => {
-    console.log("안뇽~");
+  console.log("안뇽~");
 };
 
 function App() {
-    const [inputValue1, setInputValue1] = useState("");
-    const [inputValue2, setInputValue2] = useState("");
+  const [inputValue1, setInputValue1] = useState("");
+  const [inputValue2, setInputValue2] = useState("");
 
-    const handleChange1 = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputValue1(e.target.value);
-    };
+  const handleChange1 = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue1(e.target.value);
+  };
 
-    const handleChange2 = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputValue2(e.target.value);
-    };
+  const handleChange2 = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue2(e.target.value);
+  };
 
-    return (
-        <div>
-            <GlobalStyles />
-            <StarryBackground />
-            {/* InputBox 사용 예제 */}
-            <InputBox
-                placeholder="이름을 입력해주세요"
-                type="text"
-                value={inputValue1}
-                onChange={handleChange1}
-                color="WhiteStroke"
-            />
-            {/* 입력된 값 표시 (옵션) */}
-            <div>{inputValue1}</div>
+  return (
+    <div>
+      <GlobalStyles />
+      <StarryBackground />
 
-            <InputBox
-                placeholder="이름을 입력해주세요"
-                type="text"
-                value={inputValue2}
-                onChange={handleChange2}
-                color="BlackStroke"
-            />
+      {/* InputBox 사용 예제 */}
+      <InputBox
+        placeholder="이름을 입력해주세요"
+        type="text"
+        value={inputValue1}
+        onChange={handleChange1}
+        color="WhiteStroke"
+      />
+      {/* 입력된 값 표시 (옵션) */}
+      <div>{inputValue1}</div>
 
-            {/* 입력된 값 표시 (옵션) */}
-            <div>{inputValue2}</div>
+      <InputBox
+        placeholder="이름을 입력해주세요"
+        type="text"
+        value={inputValue2}
+        onChange={handleChange2}
+        color="BlackStroke"
+      />
 
-            {/* 버튼 예제 */}
-            <div>
-                <Btn size={"X-Small"} color={"Purple"}>
-                    저장
-                </Btn>
-                <Btn size={"Circle"} color={"Black"}>
-                    💖
-                </Btn>
-            </div>
+      {/* 입력된 값 표시 (옵션) */}
+      <div>{inputValue2}</div>
 
-            <div></div>
-            <Wrapper size={"Standard"} color={"WhiteGhost"} padding={"Wide"}>
-                <Wrapper size={"Small"} color={"Clear"} padding={"Wide"}>
-                    투명 고스트 컨테이너
-                </Wrapper>
-                <Wrapper
-                    size={"Small"}
-                    color={"BlackGhost"}
-                    padding={"Medium"}
-                >
-                    검은색 고스트 컨테이너
-                </Wrapper>
-                <Wrapper
-                    size={"Small"}
-                    color={"WhiteGhost"}
-                    padding={"Narrow"}
-                >
-                    흰색 고스트 컨테이너
-                </Wrapper>
-                표준 컨테이너
-            </Wrapper>
+      {/* 버튼 예제 */}
+      <div>
+        <Btn size={"X-Small"} color={"Purple"}>
+          저장
+        </Btn>
+        <Btn size={"Circle"} color={"Black"}>
+          💖
+        </Btn>
+      </div>
 
-            {/* 해시태그 예제 */}
-            <div>
-                <HashTag size={"Huge"} color={"BlackGhost"}>
-                    # 메인 별자리
-                </HashTag>
-                <HashTag size={"Standard"} color={"White"}>
-                    # 수사물
-                </HashTag>
-                <HashTag size={"Standard"} color={"Black"}>
-                    # 스릴러
-                </HashTag>
-                <HashTag size={"Standard"} color={"WhiteGhost"}>
-                    # 정해인
-                </HashTag>
-                <HashTag size={"Standard"} color={"BlackGhost"}>
-                    # 김태리
-                </HashTag>
-            </div>
+      <div></div>
+      <Wrapper size={"Standard"} color={"WhiteGhost"} padding={"Wide"}>
+        <Wrapper size={"Small"} color={"Clear"} padding={"Wide"}>
+          투명 고스트 컨테이너
+        </Wrapper>
+        <Wrapper size={"Small"} color={"BlackGhost"} padding={"Medium"}>
+          검은색 고스트 컨테이너
+        </Wrapper>
+        <Wrapper size={"Small"} color={"WhiteGhost"} padding={"Narrow"}>
+          흰색 고스트 컨테이너
+        </Wrapper>
+        표준 컨테이너
+      </Wrapper>
 
-            <Img
-                size={"X-Large"}
-                src={"/assets/ㄷㅇㅅㅇ.png"}
-                onClick={sayHelloHandler}
-                // $point
-            />
-            {/* TEXT 예제 */}
-            <Text size="Large" color="White" fontFamily="PyeongChang-Bold">
-                {" "}
-                {LOGO}
-            </Text>
-            <Text size="Large" color="White" fontFamily="PyeongChang-Light">
-                {" "}
-                {MAIN_NOT_LOGIN}{" "}
-            </Text>
-            <IconBox
-                Icon={HiHeart}
-                size={24}
-                color={"white"}
-                onClick={sayHelloHandler}
-            />
-        </div>
-    );
+      {/* 해시태그 예제 */}
+      <div>
+        <HashTag size={"Huge"} color={"BlackGhost"}>
+          # 메인 별자리
+        </HashTag>
+        <HashTag size={"Standard"} color={"White"}>
+          # 수사물
+        </HashTag>
+        <HashTag size={"Standard"} color={"Black"}>
+          # 스릴러
+        </HashTag>
+        <HashTag size={"Standard"} color={"WhiteGhost"}>
+          # 정해인
+        </HashTag>
+        <HashTag size={"Standard"} color={"BlackGhost"}>
+          # 김태리
+        </HashTag>
+      </div>
+
+      <Img
+        size={"X-Large"}
+        src={"/assets/ㄷㅇㅅㅇ.png"}
+        onClick={sayHelloHandler}
+        // $point
+      />
+      {/* TEXT 예제 */}
+      <Text size="Large" color="White" fontFamily="PyeongChang-Bold">
+        {" "}
+        {LOGO}
+      </Text>
+      <Text size="Large" color="White" fontFamily="PyeongChang-Light">
+        {" "}
+        {MAIN_NOT_LOGIN}{" "}
+      </Text>
+      <IconBox
+        Icon={HiHeart}
+        size={24}
+        color={"white"}
+        onClick={sayHelloHandler}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ Front_End/front/src/commons/style/SharedStyle.tsx
@@ -0,0 +1,53 @@
+import styled, { css } from "styled-components";
+
+/** 기본 flex */
+export const FlexBase = css`
+  display: flex;
+`;
+
+/** flex + 가운데정렬 (가로) */
+export const AlignCenter = css`
+  ${FlexBase}
+  align-items: center;
+`;
+
+/** 기본 flex + 정가운데 정렬 */
+export const FlexCenter = css`
+  ${FlexBase}
+  align-items: center;
+  justify-content: center;
+`;
+
+/** flex + 가로, 가운데, 양쪽 정렬 */
+export const FlexRowBetween = css`
+  ${FlexBase}
+  justify-content: space-between;
+  align-items: center;
+`;
+
+/** flex + 세로, 가운데, 양쪽 정렬 */
+export const FlexColBetween = css`
+  ${FlexBase}
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+/** flex + 세로, 왼쪽 정렬 */
+export const FlexColBetweenLeft = css`
+  ${FlexBase}
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+/** flex + 세로, 가운데, 균등 정렬 */
+export const FlexColAround = css`
+  ${FlexBase}
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const FlexAlignBottom = css`
+  align-self: end;
+`;
