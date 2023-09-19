7e83cf3c45cc9d7b95ecee29911673ee1ccbd849
선다영 <seondy99@gmail.com>
2023-09-19 09:10:03 +0900

    S09P22B204-149 Feat: atom 컴포넌트 작성 - Img

--- Front_End/front/src/App.tsx
+++ Front_End/front/src/App.tsx
@@ -3,76 +3,89 @@ import Btn from "./components/atoms/Btn";
 import HashTag from "./components/atoms/HashTag";
 import InputBox from "./components/atoms/InputBox";
 import { GlobalStyles } from "./commons/style/GlobalStyle";
+import Img from "./components/atoms/Img";
+
+const sayHelloHandler = () => {
+    console.log("안뇽~");
+};
 
 function App() {
-  const [inputValue1, setInputValue1] = useState("");
-  const [inputValue2, setInputValue2] = useState("");
+    const [inputValue1, setInputValue1] = useState("");
+    const [inputValue2, setInputValue2] = useState("");
 
-  const handleChange1 = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue1(e.target.value);
-  };
+    const handleChange1 = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue1(e.target.value);
+    };
 
-  const handleChange2 = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue2(e.target.value);
-  };
+    const handleChange2 = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue2(e.target.value);
+    };
 
-  return (
-    <div style={{ backgroundColor: "purple" }}>
-      {/* InputBox 사용 예제 */}
-      <InputBox
-        placeholder="이름을 입력해주세요"
-        type="text"
-        value={inputValue1}
-        onChange={handleChange1}
-        color="WhiteStroke"
-      />
-      {/* 입력된 값 표시 (옵션) */}
-      <div>{inputValue1}</div>
+    return (
+        <div style={{ backgroundColor: "purple" }}>
+            {/* InputBox 사용 예제 */}
+            <InputBox
+                placeholder="이름을 입력해주세요"
+                type="text"
+                value={inputValue1}
+                onChange={handleChange1}
+                color="WhiteStroke"
+            />
+            {/* 입력된 값 표시 (옵션) */}
+            <div>{inputValue1}</div>
 
-      <InputBox
-        placeholder="이름을 입력해주세요"
-        type="text"
-        value={inputValue2}
-        onChange={handleChange2}
-        color="BlackStroke"
-      />
+            <InputBox
+                placeholder="이름을 입력해주세요"
+                type="text"
+                value={inputValue2}
+                onChange={handleChange2}
+                color="BlackStroke"
+            />
 
-      {/* 입력된 값 표시 (옵션) */}
-      <div>{inputValue2}</div>
+            {/* 입력된 값 표시 (옵션) */}
+            <div>{inputValue2}</div>
 
-      {/* 버튼 예제 */}
-      <div>
-        <GlobalStyles />
-        <Btn size={"X-Small"} color={"Purple"}>
-          저장
-        </Btn>
-        <Btn size={"Circle"} color={"Black"}>
-          💖
-        </Btn>
-      </div>
+            {/* 버튼 예제 */}
+            <div>
+                <GlobalStyles />
+                <Btn size={"X-Small"} color={"Purple"}>
+                    저장
+                </Btn>
+                <Btn size={"Circle"} color={"Black"}>
+                    💖
+                </Btn>
+            </div>
 
-      <div></div>
+            <div></div>
 
-      {/* 해시태그 예제 */}
-      <div>
-        <HashTag size={"Huge"} color={"BlackGhost"}>
-          # 메인 별자리
-        </HashTag>
-        <HashTag size={"Standard"} color={"White"}>
-          # 수사물
-        </HashTag>
-        <HashTag size={"Standard"} color={"Black"}>
-          # 스릴러
-        </HashTag>
-        <HashTag size={"Standard"} color={"WhiteGhost"}>
-          # 정해인
-        </HashTag>
-        <HashTag size={"Standard"} color={"BlackGhost"}>
-          # 김태리
-        </HashTag>
-      </div>
-    </div>
-  );
+            {/* 해시태그 예제 */}
+            <div>
+                <HashTag size={"Huge"} color={"BlackGhost"}>
+                    # 메인 별자리
+                </HashTag>
+                <HashTag size={"Standard"} color={"White"}>
+                    # 수사물
+                </HashTag>
+                <HashTag size={"Standard"} color={"Black"}>
+                    # 스릴러
+                </HashTag>
+                <HashTag size={"Standard"} color={"WhiteGhost"}>
+                    # 정해인
+                </HashTag>
+                <HashTag size={"Standard"} color={"BlackGhost"}>
+                    # 김태리
+                </HashTag>
+            </div>
+
+            <Img
+                size={"X-Large"}
+                src={"/assets/ㄷㅇㅅㅇ.png"}
+                onClick={sayHelloHandler}
+                // hover
+                $point
+            />
+        </div>
+    );
 }
 
 export default App;
