78bf94a90f09743f046c75c2946c9830955afdde
이수연 <dltd0912@gmail.com>
2023-09-20 12:54:36 +0900

    Feat: routes, organisms 생성

--- Front_End/front/src/commons/constants/Routes.tsx
+++ Front_End/front/src/commons/constants/Routes.tsx
@@ -1,1 +1,13 @@
-export {};
+export const ROUTES = {
+    MAIN: '/',
+    ADDINFO: '/addinfo',
+    SURVEY: '/survey',
+    OTTSELECT: '/ott',
+    SEARCH: '/search',
+    MYPAGE: '/mypage/:userId',
+    PROFILE: '/profile/:userId',
+    RECOMMEND: '/recommend/:userId',
+    RECOMMEND_MORE: '/recommend/more/:userId',
+    MOVIE_DETAIL: '/movie/:movieId',
+    NOTFOUND: '/*',
+  };
