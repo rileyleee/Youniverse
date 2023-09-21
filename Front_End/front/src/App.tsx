c27961ad86a2c3a2cc8316730afaa02cbe81d871
선다영 <seondy99@gmail.com>
2023-09-21 14:46:51 +0900

    Feat: 영화 아이템 hover 시 변경

--- Front_End/front/src/App.tsx
+++ Front_End/front/src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import StarryBackground from "./commons/style/StarryBackground";
 import { GlobalStyles } from "./commons/style/GlobalStyle";
-import MovieItem from "./components/movies/MovieItem";
 
 function App() {
   return (
