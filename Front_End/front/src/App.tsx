d2fb2c81cde093f0e1b1f189eddabb9c4c0c0a7f
선다영 <seondy99@gmail.com>
2023-09-21 14:29:39 +0900

    Feat: 영화 아이템(카드 컴포넌트) 작성 및 카드 컴포넌트 삭제

--- Front_End/front/src/App.tsx
+++ Front_End/front/src/App.tsx
@@ -2,14 +2,13 @@ import React from "react";
 
 import StarryBackground from "./commons/style/StarryBackground";
 import { GlobalStyles } from "./commons/style/GlobalStyle";
+import MovieItem from "./components/movies/MovieItem";
 
 function App() {
-
   return (
     <div>
       <GlobalStyles />
       <StarryBackground />
-
     </div>
   );
 }
