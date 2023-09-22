c034d34f9e5f4e8e316426743de13fa48bdcc2cd
이수연 <dltd0912@gmail.com>
2023-09-22 21:40:19 +0900

    Feat: 영화 상세페이지 풀스크린 스크롤 적용

---
+++ Front_End/front/src/components/movies/MovieDetailYouTube.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Wrapper from "../atoms/Wrapper"
+
+const MovieDetailYouTube = () => {
+    return (
+        <Wrapper size="Standard" color="WhiteGhost" padding="Narrow">
+            유튜브 영상들 보여줄 컴포넌트임 ~~
+        </Wrapper>
+    )
+}
+
+export default MovieDetailYouTube
