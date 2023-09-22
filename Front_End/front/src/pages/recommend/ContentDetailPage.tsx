3ec433b59896e4fa8aa1466b895a0ceaab843ff8
이수연 <dltd0912@gmail.com>
2023-09-22 19:40:17 +0900

    Feat: 영화 상세페이지 레이아웃

--- Front_End/front/src/pages/recommend/ContentDetailPage.tsx
+++ Front_End/front/src/pages/recommend/ContentDetailPage.tsx
@@ -1,10 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+import { FlexColAround } from "../../commons/style/SharedStyle"
+import Wrapper from "../../components/atoms/Wrapper";
+import MovieDetail from "../../components/movies/MovieDetail";
+import Review from "../../components/review/Review";
+
 const ContentDetailPage = () => {
-    return (
-      <div>
-        영화 상세페이지
-      </div>
-    );
-  }
-  
-  export default ContentDetailPage;
-  
+  return (
+    <StyledDetail>
+      <Wrapper size="Small" color="WhiteGhost" padding="Narrow">
+        <MovieDetail />
+      </Wrapper>
+      
+      <Wrapper size="Small" color="WhiteGhost" padding="Narrow">
+        <Review />
+      </Wrapper>
+    </StyledDetail>
+  );
+};
+
+export default ContentDetailPage;
+
+const StyledDetail = styled.div`
+  ${FlexColAround}
+`
