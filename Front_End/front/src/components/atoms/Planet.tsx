c147a66e15f1d194e8fa83b73cd8ffd276e44d17
이은경 <eunclid0202@gmail.com>
2023-09-22 10:32:04 +0900

    S09P22B204-85 Feature: OTT 행성 atom 생성

---
+++ Front_End/front/src/components/atoms/Planet.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { styled } from "styled-components";
+
+/** 행성 타입 지정 */
+interface PlanetProps {
+  size: PlanetSize;
+  src: string;
+  $point?: boolean; // 클릭 여부 확인
+  onClick?: () => void;
+}
+
+/** 행성 SIZE */
+type PlanetSize = "Standard" | "Small";
+
+/** 행성 STYLE 타입 지정 */
+type PlanetStyle = {
+  height: string;
+};
+
+/** 행성 STYLE */
+const PlanetStyles: Record<PlanetSize, PlanetStyle> = {
+  Standard: {
+    height: "248px",
+  },
+  Small: {
+    height: "24px",
+  },
+};
+
+/** styled-component => Planet */
+const StyledPlanet = styled.div<PlanetProps & { selected: boolean }>`
+  width: ${(props) => PlanetStyles[props.size].height};
+  height: ${(props) => PlanetStyles[props.size].height};
+  overflow: hidden;
+  border-radius: 50%;
+  background: url(${(props) => props.src}) center/cover;
+  position: relative;
+  box-shadow: ${(props) =>
+    props.selected ? "0 0 10px 2px rgba(255, 255, 255, 0.9)" : "none"};
+
+  ${(props) =>
+    props.size === "Standard"
+      ? "cursor: ponter;"
+      : ""} // Standard 사이즈인 경우에만 클릭 가능함
+
+  
+
+  &:hover {
+    // 호버 시에 빛 번짐 효과
+    box-shadow: 0 0 10px 2px rgba(255, 255, 255, 0.9);
+  }
+`;
+
+/** Planet 컴포넌트 정의 */
+const Planet = ({ ...props }: PlanetProps) => {
+  const [selectPlanet, setSelectPlanet] = useState(false);
+
+  const handlePlanetClick = () => {
+    setSelectPlanet(!selectPlanet);
+  };
+
+  return (
+    <StyledPlanet
+      {...props}
+      selected={selectPlanet}
+      onClick={handlePlanetClick}
+    />
+  );
+};
+
+export default Planet;
