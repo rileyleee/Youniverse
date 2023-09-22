83bbb37e4dab2f79c2005aad118125066853a472
이수연 <dltd0912@gmail.com>
2023-09-23 00:26:09 +0900

    Feat: 추천페이지 텍스트 추가

--- Front_End/front/src/components/movies/MovieItemList.tsx
+++ Front_End/front/src/components/movies/MovieItemList.tsx
@@ -2,15 +2,16 @@ import React from "react";
 
 type Props = {
   filterOTT?: string | null;
+  listType?: string;
 };
 
-const MovieItemList: React.FC<Props> = ({ filterOTT }) => {
+const MovieItemList: React.FC<Props> = ({ filterOTT, listType  }) => {
   // 필요한 경우 filterOTT 값을 사용하여 영화 목록을 필터링하면 됩니다.
 
   return (
     <>
-      <div>MovieItemList 인 데 요</div>
       {filterOTT}
+      <div>{listType}</div>
     </>
   );
 };
