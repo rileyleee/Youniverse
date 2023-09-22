c6fbede4b77b2d296a19568208328fca8e18c6c6
이수연 <dltd0912@gmail.com>
2023-09-22 22:12:42 +0900

    Modify: Dropdown String 수정

--- Front_End/front/src/components/movies/RecommendYouTube.tsx
+++ Front_End/front/src/components/movies/RecommendYouTube.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 
+import {
+  RECOMMEND_PAGE_CONTAINER_KEYWORD,
+  RECOMMEND_PAGE_CONTAINER_YOUTUBE,
+} from "../../commons/constants/String";
+
 import Wrapper from "../atoms/Wrapper";
+import Text from "../atoms/Text";
 
 const RecommendYouTube = () => {
   return (
     <Wrapper size="YouTube" color="WhiteGhost" padding="Narrow">
-      <div>유튜브 키워드</div>
-      <div>유튜브 정보</div>
+      <Text size="Medium" color="White" fontFamily="PyeongChang-Light">
+        {RECOMMEND_PAGE_CONTAINER_KEYWORD}
+      </Text>
+      <Text size="Medium" color="White" fontFamily="PyeongChang-Light">
+        {RECOMMEND_PAGE_CONTAINER_YOUTUBE}
+      </Text>
     </Wrapper>
   );
 };
