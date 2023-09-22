53e85d62f7af6e62389e7734f26b23ca51f6ef63
이수연 <dltd0912@gmail.com>
2023-09-22 13:03:31 +0900

    Feat: 추천페이지 구성

--- Front_End/front/src/pages/main/MainPage.tsx
+++ Front_End/front/src/pages/main/MainPage.tsx
@@ -14,6 +14,7 @@ import {
 const MainPage = () => {
   return (
     <>
+      {/* 로그인 안했을 때 */}
       <StyeldFlexColBetween>
         <div>
           <Text size="X-Large" color="White" fontFamily="PyeongChang-Bold">
