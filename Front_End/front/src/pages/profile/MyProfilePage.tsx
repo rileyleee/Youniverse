62ae3c3a3cf8563ce9046cd0fd27b83dece8474a
선다영 <seondy99@gmail.com>
2023-09-23 17:40:35 +0900

    Feat: 기본 폰트 설정, 마이페이지 유저인포 컴포넌트 생성

--- Front_End/front/src/pages/profile/MyProfilePage.tsx
+++ Front_End/front/src/pages/profile/MyProfilePage.tsx
@@ -1,11 +1,11 @@
-import SoulMovieItem from "../../components/movies/SoulMovieItem";
 import SoulMovieItemList from "../../components/movies/SoulMovieItemList";
+import MypageUserInfo from "../../components/users/MypageUserInfo";
 
 const MyProfilePage = () => {
   return (
     <div>
       마이페이지
-      
+      <MypageUserInfo />
       <SoulMovieItemList />
     </div>
   );
