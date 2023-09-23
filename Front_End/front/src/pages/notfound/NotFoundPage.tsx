98c7dc9057275164e0e1564779698ff164f129eb
이수연 <dltd0912@gmail.com>
2023-09-23 17:45:12 +0900

    Feat: ChartJS 예제 파일 생성, Wrapper 수정

--- Front_End/front/src/pages/notfound/NotFoundPage.tsx
+++ Front_End/front/src/pages/notfound/NotFoundPage.tsx
@@ -1,10 +1,23 @@
+import Text from "../../components/atoms/Text";
+import { AlignCenter } from "../../commons/style/SharedStyle";
+import styled from "styled-components";
+
 const NotFoundPage = () => {
-    return (
-      <div>
-        NotFound
-      </div>
-    );
-  }
-  
-  export default NotFoundPage;
-  
+  return (
+    <StyledNotFound>
+      <Text size="X-Large" color="White" fontFamily="PyeongChang-Bold">
+        404 NotFound
+      </Text>
+    </StyledNotFound>
+  );
+};
+
+export default NotFoundPage;
+
+const StyledNotFound = styled.div`
+  ${AlignCenter}
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 70px); 
+`;
