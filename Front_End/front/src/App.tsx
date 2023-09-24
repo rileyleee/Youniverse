32d9ac097b3d7b7687801c6b59734f7fc142ed9e
이은경 <eunclid0202@gmail.com>
2023-09-24 16:22:28 +0900

    Feature: 추천 받은 유저 리스트 화면 작성

--- Front_End/front/src/App.tsx
+++ Front_End/front/src/App.tsx
@@ -19,6 +19,7 @@ import {
   MOVIE_DETAIL,
   NOTFOUND,
 } from "./pages/Pages";
+import UserRecommendForm from "./components/organisms/UserRecommendForm";
 
 function App() {
   return (
@@ -27,23 +28,23 @@ function App() {
         <GlobalStyles />
         <StarryBackground />
         <Header />
+        <UserRecommendForm />
 
-      <Routes>
-        <Route path={ROUTES.MAIN} Component={MAIN} />
-        <Route path={ROUTES.ADDINFO} Component={ADDINFO} />
-        <Route path={ROUTES.SURVEY} Component={SURVEY} />
-        <Route path={ROUTES.OTTSELECT} Component={OTTSELECT} />
-        <Route path={ROUTES.MYPAGE} Component={MYPAGE} />
-        <Route path={ROUTES.PROFILE} Component={PROFILE} />
-        <Route path={ROUTES.SEARCH} Component={SEARCH} />
-        <Route path={ROUTES.RECOMMEND} Component={RECOMMEND} />
-        <Route path={ROUTES.RECOMMEND_MORE} Component={RECOMMEND_MORE} />
-        <Route path={ROUTES.MOVIE_DETAIL} Component={MOVIE_DETAIL} />
-        <Route path={ROUTES.NOTFOUND} Component={NOTFOUND} />
-      </Routes>
-
-
-    </Router>
+        <Routes>
+          <Route path={ROUTES.MAIN} Component={MAIN} />
+          <Route path={ROUTES.ADDINFO} Component={ADDINFO} />
+          <Route path={ROUTES.SURVEY} Component={SURVEY} />
+          <Route path={ROUTES.OTTSELECT} Component={OTTSELECT} />
+          <Route path={ROUTES.MYPAGE} Component={MYPAGE} />
+          <Route path={ROUTES.PROFILE} Component={PROFILE} />
+          <Route path={ROUTES.SEARCH} Component={SEARCH} />
+          <Route path={ROUTES.RECOMMEND} Component={RECOMMEND} />
+          <Route path={ROUTES.RECOMMEND_MORE} Component={RECOMMEND_MORE} />
+          <Route path={ROUTES.MOVIE_DETAIL} Component={MOVIE_DETAIL} />
+          <Route path={ROUTES.NOTFOUND} Component={NOTFOUND} />
+        </Routes>
+      </Router>
+    </RecoilRoot>
   );
 }
 
