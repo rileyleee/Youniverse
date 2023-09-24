1c4704d975c37213cf4ffa8c78fae45234fa73c1
이은경 <whddlto2782@gmail.com>
2023-09-24 15:01:14 +0900

    S09P22B204-160 Feat: 유저검색 시 드롭다운 선택 값 반영

--- Front_End/front/src/components/organisms/SearchBox.tsx
+++ Front_End/front/src/components/organisms/SearchBox.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { HiSearch } from "react-icons/hi";
-
 import { SEARCH_PLACEHOLDER } from "./../../commons/constants/String";
 import { FlexCenter } from "./../../commons/style/SharedStyle";
 import Dropdown from "../@commons/Dropdown";
@@ -13,7 +12,7 @@ import IconBox from "../atoms/IconBox";
 interface SearchBoxProps {
   theme?: "light" | "dark"; // 테마 타입 (기본값 light)
   type?: "user" | "movie"; // 검색 옵션의 타입 (기본값 user)
-  onSearch: (term: string, option: string | null) => void; // 은경 반영
+  onSearch?: (term: string, option: string | null) => void; // 은경 반영
 }
 
 // 테마에 따른 스타일 값을 저장하는 객체
@@ -52,15 +51,15 @@ const MOVIE_OPTIONS = [
 const SearchBox: React.FC<SearchBoxProps> = ({
   theme = "light",
   type = "user",
-  onSearch, // 은경 반영
+  onSearch,
 }) => {
-  const [term, setTerm] = useState(""); // 은경 반영
-  const [selectedOption, setSelectedOption] = useState(null); // 은경 반영
+  const [term, setTerm] = useState("");
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
   const { inputColor, btnColor, iconColor } = THEME_STYLES[theme];
   const options = type === "user" ? USER_OPTIONS : MOVIE_OPTIONS;
 
   const handleSearchClick = () => {
-    onSearch(term, selectedOption);
+    onSearch?.(term, selectedOption);
   };
 
   return (
@@ -68,14 +67,16 @@ const SearchBox: React.FC<SearchBoxProps> = ({
       <Dropdown
         options={options}
         theme={theme}
-        onSelectedChange={(selected) => console.log(selected)} // 이 부분은 콘솔 로그 대신 원하는 로직으로 변경하세요.
+        onSelectedChange={(selected) => {
+          setSelectedOption(selected?.value ?? null); // 선택된 값이 없다면 null로 설정
+        }}
       />
       <InputBox
         placeholder={SEARCH_PLACEHOLDER}
         type="text"
         color={inputColor}
-        value={term} // 은경 반영
-        onChange={(e) => setTerm(e.target.value)} // 은경 반영
+        value={term}
+        onChange={(e) => setTerm(e.target.value)}
       />
       <Btn size={"Medium"} color={btnColor} onClick={handleSearchClick}>
         <IconBox Icon={HiSearch} size={24} color={iconColor} />
