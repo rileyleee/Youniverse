613c48046d4a7bc35a31d16f0ec0e0afc63f49c4
선다영 <seondy99@gmail.com>
2023-09-24 18:44:31 +0900

    Feat: axios 정리

--- Front_End/front/src/commons/style/GlobalStyle.tsx
+++ Front_End/front/src/commons/style/GlobalStyle.tsx
@@ -9,6 +9,21 @@ export const GlobalStyles = createGlobalStyle`
         font-family: "YESGothic-Regular";
     }
 
+    /* 임시 배경 */
+    html, body {
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(
+        180deg,
+        #190a37 0%,
+        rgba(83, 22, 132, 0.54) 60.42%,
+        rgba(194, 180, 222, 0.3) 99.97%,
+        rgba(176, 164, 202, 0.25) 99.98%,
+        rgba(150, 123, 208, 0.18) 99.99%,
+        rgba(145, 114, 211, 0) 100%
+  );
+    }
+
     @font-face {
         font-family: 'PyeongChangPeace-Bold';
         src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2206-02@1.0/PyeongChangPeace-Bold.woff2') format('woff2');
