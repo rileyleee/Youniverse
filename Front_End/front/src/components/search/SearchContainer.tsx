026ba9e3a4c18752941207ea42ad0821ec6b32ff
이수연 <dltd0912@gmail.com>
2023-09-24 16:59:07 +0900

    Feat: 검색페이지, inputBox 수정

--- Front_End/front/src/components/search/SearchContainer.tsx
+++ Front_End/front/src/components/search/SearchContainer.tsx
@@ -1,20 +1,35 @@
-import { useLocation } from "react-router-dom"; // 추가
-import {SEARCH_PAGE} from "../../commons/constants/String"
+import { useLocation, useNavigate } from "react-router-dom"; // useNavigate 추가
+import { useState } from 'react';
 
-import Wrapper from "../atoms/Wrapper"
-import SearchBox from "../organisms/SearchBox"
-import Text from "../atoms/Text"
+import { SEARCH_PAGE } from "../../commons/constants/String";
+import Wrapper from "../atoms/Wrapper";
+import SearchBox from "../organisms/SearchBox";
+import Text from "../atoms/Text";
 
 const SearchContainer = () => {
-    const location = useLocation();
-    const searchTerm = location.state?.searchTerm ?? '';
+  const location = useLocation();
+  const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 함수를 가져옵니다.
 
-    return (
-        <Wrapper size="Standard" color="WhiteGhost" padding="Medium">
-            <Text size="Large" color="Black" fontFamily="YESGothic-Bold">{searchTerm} {SEARCH_PAGE}</Text>
-            <SearchBox theme="dark" />
-        </Wrapper>
-    )
-}
+  const initialSearchTerm = location.state?.searchTerm ?? "";
+  const [searchTerm, setSearchTerm] = useState(initialSearchTerm);
 
-export default SearchContainer
+  // 검색 콜백 함수
+  const handleSearch = (term: string) => {
+    setSearchTerm(term);
+    // 여기에서 추가적인 검색 로직을 수행할 수 있습니다.
+
+    // navigate를 사용하여 현재 경로로 업데이트된 검색어를 포함한 상태를 푸시합니다.
+    navigate(location.pathname, { state: { searchTerm: term } });
+  };
+
+  return (
+    <Wrapper size="Standard" color="WhiteGhost" padding="Medium">
+      <Text size="Large" color="Black" fontFamily="YESGothic-Bold">
+        {searchTerm} {SEARCH_PAGE}
+      </Text>
+      <SearchBox theme="dark" onSubmitSearch={handleSearch} />
+    </Wrapper>
+  );
+};
+
+export default SearchContainer;
