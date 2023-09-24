c1729fb9852882c1c5c12f9a3308f612d9bd8a66
이은경 <whddlto2782@gmail.com>
2023-09-24 11:00:25 +0900

    S09P22B204-160 Feat: 특정 유저 클릭 시 유저 정보 요청 및 스타일 변경 적용

--- Front_End/front/src/App.tsx
+++ Front_End/front/src/App.tsx
@@ -18,15 +18,13 @@ import {
   NOTFOUND,
 } from "./pages/Pages";
 
-import SearchUserItemList from "./components/users/SearchUserItemList";
-
 function App() {
   return (
     <Router>
       <GlobalStyles />
       <StarryBackground />
       <Header />
-      <SearchUserItemList />
+
       <Routes>
         <Route path={ROUTES.MAIN} Component={MAIN} />
         <Route path={ROUTES.ADDINFO} Component={ADDINFO} />
