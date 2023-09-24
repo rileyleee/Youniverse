a830be4a9ffd79879c42628e5f0c8fb5c5ad42ef
이은경 <whddlto2782@gmail.com>
2023-09-24 10:12:33 +0900

    S09P22B204-160 Feat: 유저 검색 시 검색 결과 저장하고 그 수 만큼 유저 컴포넌트 생성 구현

--- Front_End/front/src/components/users/SearchUserItem.tsx
+++ Front_End/front/src/components/users/SearchUserItem.tsx
@@ -1,26 +1,41 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 import Img from "../atoms/Img";
 import HashTag from "../atoms/HashTag";
 import { FlexCenter } from "../../commons/style/SharedStyle";
 
-const SearchUserItem = () => {
+type UserProps = {
+  id: number;
+  nickname: string;
+  image: string;
+  hashtags: string[];
+};
+
+const SearchUserItem = ({ user }: { user: UserProps }) => {
+  const navigate = useNavigate();
+  const { id, nickname, image, hashtags } = user;
+  /**클릭 시 사용자 ID를 이용해 프로필 이동 */
   const handleToClickedUser = () => {
-    // 사용자 ID를 이용해 프로필 이동
     try {
-    } catch (error) {}
+      navigate(`/profile/${id}`);
+    } catch (error) {
+      console.error("사용자 프로필로 이동할 수 없음:", error);
+    }
   };
   return (
     <StyledUserContainer>
-      <StyledCenterContainer>
-        <Img
-          size="Medium"
-          src="assets/Logo/GoogleLogo.svg"
-          onClick={handleToClickedUser}
-        />
+      <StyledCenterContainer onClick={handleToClickedUser}>
+        <Img size="Medium" src={image} />
         <div>
-          <div>닉네임</div>
-          <div>키워드 리스트 해시태그 형태</div>
+          <div>{nickname}</div>
+          <div>
+            {hashtags.map((hashtag, index) => (
+              <HashTag key={index} size="Standard" color="White">
+                {hashtag}
+              </HashTag>
+            ))}
+          </div>
         </div>
       </StyledCenterContainer>
     </StyledUserContainer>
--- Front_End/front/src/components/users/SearchUserItemList.tsx
+++ Front_End/front/src/components/users/SearchUserItemList.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
+import axios from "axios";
 import { SEARCH_USER_RECOMMEND } from "../../commons/constants/String";
 import Wrapper from "../atoms/Wrapper";
 import Text from "../atoms/Text";
@@ -7,6 +8,24 @@ import SearchUserItem from "./SearchUserItem";
 import { FlexCenter, FlexColBetween } from "../../commons/style/SharedStyle";
 
 const SearchUserItemList = () => {
+  // 로그인된 유저 정보 -> ID, 닉네임
+
+  /**백 서버에서 받아온 유저 리스트 저장 */
+  const [searchResultUsers, setSearchResultUsers] = useState([]);
+
+  useEffect(() => {
+    const SearchResultUserList = async () => {
+      try {
+        const response = await axios.get("api 주소");
+        setSearchResultUsers(response.data.users); //백에서 넘어오는 데이터 확인
+      } catch (error) {
+        console.error("데이터 가져오기 실패", error);
+      }
+    };
+
+    SearchResultUserList();
+  }, []);
+
   return (
     <StyledStandardWhiteGhostWrapper
       size="Standard"
@@ -17,7 +36,9 @@ const SearchUserItemList = () => {
         <Text size="Small" color="Black" fontFamily="YESGothic-Regular">
           {SEARCH_USER_RECOMMEND}
         </Text>
-        <SearchUserItem />
+        {searchResultUsers.map((user, index) => (
+          <SearchUserItem key={index} user={user} />
+        ))}
       </StyledCenter>
     </StyledStandardWhiteGhostWrapper>
   );
