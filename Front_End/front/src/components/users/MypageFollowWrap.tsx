71a3f796828c371df17e2af2509ecdd18a5f1625
선다영 <seondy99@gmail.com>
2023-09-25 14:42:07 +0900

    S09P22B204-195 Feat: 별자리 컴포넌트 작성 및 팔로잉 팔로우 링크 설정

---
+++ Front_End/front/src/components/users/MypageFollowWrap.tsx
@@ -0,0 +1,84 @@
+// 마이페이지에서 팔로우, 팔로워 눌렀을 때 우측에 보이는 wrapper
+
+// import { useState } from "react";
+import Text from "../atoms/Text";
+import Btn from "../atoms/Btn";
+import {
+  SEARCH_USER,
+  FOLLOWING,
+  FOLLOWER,
+} from "../../commons/constants/String";
+import Wrapper from "../atoms/Wrapper";
+import styled from "styled-components";
+import IconBox from "../atoms/IconBox";
+import { HiChevronLeft } from "react-icons/hi";
+
+interface MypageFollowProps {
+  followStatus: string;
+  setFollowStatus: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const MypageFollowWrap: React.FC<MypageFollowProps> = ({
+  followStatus,
+  setFollowStatus,
+}) => {
+  // const [followStatus, setFollowStatus] = useState(""); // 누른 것에 따라 보여줌
+
+  return (
+    <Wrapper size="Standard" color="WhiteGhost" padding="Medium">
+      {/* 팔로잉, 팔로워, 새로운 유저 찾기 */}
+      <div>
+        <div>
+          <IconBox Icon={HiChevronLeft} size={32} color="Black" onClick={() => setFollowStatus("")} />
+          <div>
+            <Text
+              size="Medium"
+              color={followStatus === FOLLOWING ? "Black" : "Gray"}
+              fontFamily="YESGothic-Bold"
+              onClick={() => setFollowStatus(FOLLOWING)}
+            >
+              {FOLLOWING}
+            </Text>
+            <StyledCircleNumber isActive={followStatus === FOLLOWING}>
+              5
+            </StyledCircleNumber>
+          </div>
+          <div>
+            <Text
+              size="Medium"
+              color={followStatus === FOLLOWER ? "Black" : "Gray"}
+              fontFamily="YESGothic-Bold"
+              onClick={() => setFollowStatus(FOLLOWER)}
+            >
+              {FOLLOWER}
+            </Text>
+            <StyledCircleNumber isActive={followStatus === FOLLOWER}>
+              5
+            </StyledCircleNumber>
+          </div>
+        </div>
+        <Btn size="Small" color="White">
+          {SEARCH_USER}
+        </Btn>
+      </div>
+
+      {/* 팔로잉 / 팔로워 목록 보여주는 공간 */}
+      {followStatus === FOLLOWING && "팔로잉 목록 보여줘용"}
+      {followStatus === FOLLOWER && "팔로워 목록 보여줘용"}
+    </Wrapper>
+  );
+};
+
+export default MypageFollowWrap;
+
+export const StyledCircleNumber = styled.div<{ isActive?: boolean }>`
+  width: 24px;
+  height: 24px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  font-weight: bold;
+  font-size: 12px;
+  border-radius: 50%;
+  background-color: ${(props) => (props.isActive ? "#000" : "#ccc")};
+`;
--- Front_End/front/src/pages/profile/MyProfilePage.tsx
+++ Front_End/front/src/pages/profile/MyProfilePage.tsx
@@ -1,19 +1,36 @@
+import { useState } from "react";
 import { MainPaddingContainer } from "../../commons/style/layoutStyle";
 import SoulMovieItemList from "../../components/movies/SoulMovieItemList";
 import MyOTTPlanet from "../../components/users/MyOTTPlanet";
+import MypageFollowWrap from "../../components/users/MypageFollowWrap";
 import MypageLikeContents from "../../components/users/MypageLikeContents";
 import MypageUserInfo from "../../components/users/MypageUserInfo";
+import UserZodiacSign from "../../components/users/UserZodiacSign";
 
 const MyProfilePage = () => {
+  const [followStatus, setFollowStatus] = useState<string>("");
   return (
     <MainPaddingContainer>
-      <div className="grid grid-cols-3 gap-4">
-        <MypageUserInfo />
-        <div>
-          <SoulMovieItemList />
-          <MyOTTPlanet />
-          <MypageLikeContents />
-        </div>
+      <div className="grid grid-cols-2 gap-4">
+        <MypageUserInfo
+          followStatus={followStatus}
+          setFollowStatus={setFollowStatus}
+        />
+        {!followStatus && (
+          <div>
+            {/* <SoulMovieItemList />
+            <MyOTTPlanet />
+            <MypageLikeContents /> */}
+            <UserZodiacSign />
+          </div>
+        )}
+
+        {followStatus && (
+          <MypageFollowWrap
+            followStatus={followStatus}
+            setFollowStatus={setFollowStatus}
+          />
+        )}
       </div>
     </MainPaddingContainer>
   );
