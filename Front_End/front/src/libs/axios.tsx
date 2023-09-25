4273881bf055d2a2167f67c341c5285e03c5eb4a
이은경 <whddlto2782@gmail.com>
2023-09-26 00:37:45 +0900

    Fix: axios 요청 주소 변경

--- Front_End/front/src/libs/axios.tsx
+++ Front_End/front/src/libs/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const { REACT_APP_SERVER_URL} = process.env;
+const { REACT_APP_SERVER_URL } = process.env;
 
 export const mainAxios = axios.create({
-  baseURL: `${REACT_APP_SERVER_URL}/`
-}); 
+  baseURL: `${REACT_APP_SERVER_URL}/api`,
+});
