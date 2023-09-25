e3e9f01b18b7dd210da026ef7c7fcde2c47b06f4
이수연 <dltd0912@gmail.com>
2023-09-25 21:55:23 +0900

    Feat: axios 수정, 영화 상세페이지 axios 요청

--- Front_End/front/src/components/chart/LineChartWrapper.tsx
+++ Front_End/front/src/components/chart/LineChartWrapper.tsx
@@ -1,22 +1,58 @@
-import Wrapper, { WrapperSize } from "../atoms/Wrapper";
+// import Wrapper from "../atoms/Wrapper";
+// import LineChart from "./LineChart";
+
+// interface LineChartWrapperProps {
+//   chartWidth?: string | number; // 수정
+//   chartHeight?: string | number; // 수정
+// }
+
+// const LineChartWrapper: React.FC<LineChartWrapperProps> = () => {
+//   return (
+//     <Wrapper size="Standard" color="WhiteGhost" padding="Narrow">
+//       <LineChart />
+//     </Wrapper>
+//   );
+// };
+
+// export default LineChartWrapper;
+// 유저의 별자리를 보여주는 컴포넌트
+
+import styled from "styled-components";
+import Wrapper from "../atoms/Wrapper";
 import LineChart from "./LineChart";
+import { FlexCenter } from "../../commons/style/SharedStyle";
 
 interface LineChartWrapperProps {
-  size?: WrapperSize;
-  chartWidth?: number; // 추가
-  chartHeight?: number; // 추가
+  chartWidth?: string | number; // 수정
+  chartHeight?: string | number; // 수정
 }
-
-const LineChartWrapper: React.FC<LineChartWrapperProps> = ({
-  size = "RecommendedChart",
-  chartWidth = 400, // 기본값 설정
-  chartHeight = 200, // 기본값 설정
-}) => {
+const LineChartWrapper: React.FC<LineChartWrapperProps> = () => {
   return (
-    <Wrapper size={size} color="WhiteGhost" padding="Wide">
-      <LineChart width={chartWidth} height={chartHeight} />
-    </Wrapper>
+    <>
+      <Wrapper size="Standard" color="WhiteGhost" padding="Narrow">
+        <StyledZodiacWrapper>
+          <LineChart />
+        </StyledZodiacWrapper>
+      </Wrapper>
+    </>
   );
 };
 
 export default LineChartWrapper;
+
+/** 별자리 차트 감싸는 그라디언트 박스 */
+const StyledZodiacWrapper = styled.div`
+  ${FlexCenter}
+  width: 100%;
+  height: 100%;
+  border-radius: 28px;
+  background: linear-gradient(
+    180deg,
+    #190a37 0%,
+    rgba(83, 22, 132, 0.54) 60.42%,
+    rgba(194, 180, 222, 0.3) 99.97%,
+    rgba(176, 164, 202, 0.25) 99.98%,
+    rgba(150, 123, 208, 0.18) 99.99%,
+    rgba(145, 114, 211, 0) 100%
+  );
+`;
--- Front_End/front/src/components/movies/RecommendYouTube.tsx
+++ Front_End/front/src/components/movies/RecommendYouTube.tsx
@@ -5,28 +5,43 @@ import {
   RECOMMEND_PAGE_CONTAINER_YOUTUBE,
 } from "../../commons/constants/String";
 
+import {
+  FlexRowBetween,
+} from "../../commons/style/SharedStyle";
+
 import Wrapper from "../atoms/Wrapper";
 import Text from "../atoms/Text";
 import LineChartWrapper from "../chart/LineChartWrapper";
+import styled from "styled-components";
 
 const RecommendYouTube = () => {
   return (
-    <Wrapper size="YouTube" color="WhiteGhost" padding="Narrow">
-      <div>
+    <StyledWrapper size="YouTube" color="WhiteGhost" padding="Narrow">
+      <StyledYouTubeStar>
         <Text size="Medium" color="White" fontFamily="PyeongChang-Light">
           {RECOMMEND_PAGE_CONTAINER_KEYWORD}
         </Text>
-        <LineChartWrapper
-          size="RecommendedChart"
-          chartWidth={220}
-          chartHeight={110}
-        />
-      </div>
-      <Text size="Medium" color="White" fontFamily="PyeongChang-Light">
-        {RECOMMEND_PAGE_CONTAINER_YOUTUBE}
-      </Text>
-    </Wrapper>
+        <LineChartWrapper chartWidth="100%" chartHeight="100%" />
+      </StyledYouTubeStar>
+      <StyledYouTubeRec>
+        <Text size="Medium" color="White" fontFamily="PyeongChang-Light">
+          {RECOMMEND_PAGE_CONTAINER_YOUTUBE}
+        </Text>
+      </StyledYouTubeRec>
+    </StyledWrapper>
   );
 };
 
 export default RecommendYouTube;
+
+const StyledWrapper = styled(Wrapper)`
+  ${FlexRowBetween}
+`;
+
+const StyledYouTubeStar = styled.div`
+  width: 40%;
+`;
+
+const StyledYouTubeRec = styled.div`
+  width: 56%;
+`;
