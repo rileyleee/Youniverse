cb2d54cf9dc103ef59279c8b53d4c9875dbc0a6d
이수연 <dltd0912@gmail.com>
2023-09-26 09:42:26 +0900

    Feat: 영화 전체데이터 가져옴

--- Front_End/front/src/components/movies/MovieItemList.tsx
+++ Front_End/front/src/components/movies/MovieItemList.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-import { FlexRowBetween } from "../../commons/style/SharedStyle";
+import { FlexRowBetween, AlignCenter } from "../../commons/style/SharedStyle";
 import Btn from "../atoms/Btn";
 import Text from "../atoms/Text";
 import MovieItem from "./MovieItem";
@@ -14,14 +14,23 @@ type Props = {
   listType?: string;
 };
 
-const MovieItemList: React.FC<Props> = ({ filterOTT, listType,  }) => {
+export type MovieType = {
+  movieId: number;
+  title: string;
+  movieImage: string;
+  rate: number;
+  runtime: number;
+  // 필요한 다른 필드들 추가
+};
+
+const MovieItemList: React.FC<Props> = ({ filterOTT, listType }) => {
   // 필요한 경우 filterOTT 값을 사용하여 영화 목록을 필터링하면 됩니다.
   // listType: 유튜브 기반 추천, @@님의 선호도 기반, @@@님의 인생영화 ... 이런 텍스트
   // filterOTT: 추천 -> 더보기 들어갔을 때 OTT 선택해서 필터링 하는거
   // 검색결과에 맞는 영화 추가 필요
   const navigate = useNavigate();
 
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState<MovieType[]>([]);
 
   const handleMoreClick = (userId: number) => {
     navigate(`/recommend/more/${userId}`);
@@ -30,8 +39,8 @@ const MovieItemList: React.FC<Props> = ({ filterOTT, listType,  }) => {
   useEffect(() => {
     getAllMovies()
       .then((response) => {
-        console.log(response.data);
-        setMovies(response.data);
+        console.log(response.data.content);
+        setMovies(response.data.content);
       })
       .catch((err) => {
         console.log(err);
@@ -56,11 +65,13 @@ const MovieItemList: React.FC<Props> = ({ filterOTT, listType,  }) => {
           </StyledBtn>
         )}
       </StyledListBtn>
-      {/* {movies.map((movie) => (
-        <MovieItem key={movie} movie={movie} />
-      ))} */}
-      {movies}
-      <MovieItem />
+      <StyledMovieItem>
+        {movies.map((movie) => (
+          <MovieItem key={(movie as MovieType).movieId} movie={movie} />
+        ))}
+      </StyledMovieItem>
+      {/* {movies} */}
+      {/* <MovieItem /> */}
     </>
   );
 };
@@ -73,4 +84,8 @@ const StyledListBtn = styled.div`
 
 const StyledBtn = styled(Btn)`
   width: 100px;
+`;
+
+const StyledMovieItem = styled.div`
+  ${AlignCenter}
 `;
--- Front_End/front/src/components/users/MypageLikeContents.tsx
+++ Front_End/front/src/components/users/MypageLikeContents.tsx
@@ -1,11 +1,26 @@
 // 마이페이지에 있는 ㅇㅇ 님이 좋아한 콘텐츠 컴포넌트
-
+import React, { useState, useEffect } from "react";
 import { MY_PAGE_LIKE } from "../../commons/constants/String";
+import { getAllMovies } from "../../apis/FrontendApi";
 import Text from "../atoms/Text";
 import Wrapper from "../atoms/Wrapper";
+import { MovieType } from "../movies/MovieItemList";
 import MovieItem from "../movies/MovieItem";
 
 const MypageLikeContents = () => {
+  const [movies, setMovies] = useState<MovieType[]>([]);
+
+  // 영화 데이터를 불러오는 로직
+  useEffect(() => {
+    getAllMovies()
+      .then((response) => {
+        console.log(response.data.content);
+        setMovies(response.data.content);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div>
       <div>
@@ -19,9 +34,12 @@ const MypageLikeContents = () => {
       {/* 좋아한 콘텐츠 Wrapper movieitem 컴포넌트 및 마지막 컴포넌트 수정 @@@ */}
       <Wrapper size="Standard" color="WhiteGhost" padding="Narrow">
         <div className="grid grid-cols-3">
+          {movies.map((movie) => (
+            <MovieItem key={movie.movieId} movie={movie} />
+          ))}
+          {/* <MovieItem />
           <MovieItem />
-          <MovieItem />
-          <MovieItem />
+          <MovieItem /> */}
         </div>
       </Wrapper>
     </div>
