18f5744b2008b166acd9c7dd2a58f6848bc54ef2
선다영 <seondy99@gmail.com>
2023-09-26 09:52:47 +0900

    Feat: 영화 아이템 리스트, 영화 디테일 컴포넌트 일부 임시 수정

--- Front_End/front/src/libs/axios.tsx
+++ Front_End/front/src/libs/axios.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const { REACT_APP_SERVER_URL} = process.env;
+const { REACT_APP_SERVER_URL } = process.env;
 
 export const mainAxios = axios.create({
   baseURL: `${REACT_APP_SERVER_URL}/api`
