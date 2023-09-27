8d1227dc5b3749e4a91b2c2508559e04b15b85cd
이은경 <eunclid0202@gmail.com>
2023-09-28 02:58:18 +0900

    Feat: 리뷰 react 슬라이더 반영

--- Front_End/front/src/components/review/ProfileReviewItemList.tsx
+++ Front_End/front/src/components/review/ProfileReviewItemList.tsx
@@ -1,3 +1,7 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import ProfileReviewItem from "./ProfileReviewItem";
 
 // 프로필에서 보여지는 리뷰 여러 개
@@ -27,13 +31,27 @@ interface Props {
   reviews: ReviewItem[];
 }
 
+const settings = {
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  vertical: true,
+  arrows: false,
+};
+
 const ProfileReviewItemList: React.FC<Props> = ({ reviews }) => {
   return (
-    <div>
+    <Slider {...settings}>
       {reviews.map((review) => (
-        <ProfileReviewItem key={review.reviewId} review={review} />
+        <div key={review.reviewId}>
+          <ProfileReviewItem review={review} />
+        </div>
       ))}
-    </div>
+    </Slider>
   );
 };
 
