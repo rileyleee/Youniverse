cc191bcc4666d1846224c82c3c37f4ca40fd9aa2
이은경 <eunclid0202@gmail.com>
2023-09-28 02:28:05 +0900

    Feat: 사용자 작성 리뷰 조회 컴포넌트

--- Front_End/front/src/components/review/ProfileReview.tsx
+++ Front_End/front/src/components/review/ProfileReview.tsx
@@ -1,16 +1,39 @@
 // 프로필에서 보여지는 리뷰 박스 (+ 텍스트)
 
+import { useState, useEffect } from "react";
+import { useRecoilValue } from "recoil";
 import { MY_PAGE_REVIEW } from "../../commons/constants/String";
 import Text from "../atoms/Text";
+import { UserDetailInfoState } from "../../pages/store/State";
+import { getMember } from "../../apis/FrontendApi";
+import ProfileReviewItemList from "./ProfileReviewItemList";
 
 const ProfileReview = () => {
+  const { nickname, memberId } = useRecoilValue(UserDetailInfoState);
+  const [reviewList, setReviewList] = useState<[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getMember(memberId as number);
+
+        if (response && response.data) {
+          setReviewList(response.data.reviewResDtos || []);
+        }
+      } catch (error) {
+        console.error("데이터 가져오기 실패", error);
+      }
+    };
+
+    fetchData();
+  }, [memberId]);
   return (
     <div>
       <Text size="Medium" color="White" fontFamily="PyeongChang-Bold">
-        유저{MY_PAGE_REVIEW}
+        {nickname}
+        {MY_PAGE_REVIEW}
       </Text>
-      {/* 리뷰 wrapper */}
-      <div></div>
+      <ProfileReviewItemList reviews={reviewList} />
     </div>
   );
 };
--- Front_End/front/src/components/review/ProfileReviewItem.tsx
+++ Front_End/front/src/components/review/ProfileReviewItem.tsx
@@ -1,18 +1,47 @@
 // 프로필에서 보여지는 리뷰 한 개
+import { useState, useEffect } from "react";
+import { ReviewItem } from "./ProfileReviewItemList";
+import { MovieType } from "../../pages/recommend/ContentDetailPage";
+import { getMovie } from "../../apis/FrontendApi";
 
-const ProfileReviewItem = () => {
+interface ReviewProps {
+  review: ReviewItem;
+}
+
+const ProfileReviewItem: React.FC<ReviewProps> = ({ review }) => {
+  const {
+    movieSimpleResDto: { movieId, title },
+    reviewContent,
+    reviewRate,
+  } = review;
+  const [movieDetailInfo, setMovieDetailInfo] = useState<MovieType | null>(
+    null
+  );
+
+  useEffect(() => {
+    const MovieDetail = async () => {
+      try {
+        const response = await getMovie(movieId);
+        setMovieDetailInfo(response.data);
+      } catch (error) {
+        console.error("영화 정보 가져오기 실패:", error);
+      }
+    };
+
+    MovieDetail();
+  }, [movieId]);
+
   return (
     <>
       <div>
         <div>
-          <div>슬픔의 삼각형</div>
-          <div>장르 : 코미디, 드라마</div>
-          <div>감독 : 루벤 외스틀룬드</div>
+          <div>{title}</div>
+          <div>{movieDetailInfo?.directorResDtos[0]?.directorName}</div>
         </div>
-        <div>별 들어갈 공간</div>
+        <div>{reviewRate}</div>
       </div>
 
-      <div>여기에 리뷰 내용 들어가용</div>
+      <div>{reviewContent}</div>
     </>
   );
 };
