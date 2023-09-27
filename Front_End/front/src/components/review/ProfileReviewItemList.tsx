b4f72a7ede8173e3f5a5d2c8a343a0afd0b849a5
이은경 <whddlto2782@gmail.com>
2023-09-28 02:28:05 +0900

    Feat: 사용자 작성 리뷰 조회 컴포넌트

--- Front_End/front/src/components/review/ProfileReviewItemList.tsx
+++ Front_End/front/src/components/review/ProfileReviewItemList.tsx
@@ -1,7 +1,40 @@
-// 프로필에서 보여지는 리뷰 여러 개
+import ProfileReviewItem from "./ProfileReviewItem";
 
-const ProfileReviewItemList = () => {
-  return <div></div>;
+// 프로필에서 보여지는 리뷰 여러 개
+type Keyword = {
+  keywordId: number;
+  keywordName: string;
+  source: number;
+};
+
+type MovieSimpleResDto = {
+  movieId: number;
+  title: string;
+  movieImage: string;
+  keywordResDtos: Keyword[];
+  rate: number;
+  runtime: number;
+};
+
+export type ReviewItem = {
+  reviewId: number;
+  movieSimpleResDto: MovieSimpleResDto;
+  reviewContent: string;
+  reviewRate: number;
+};
+
+interface Props {
+  reviews: ReviewItem[];
+}
+
+const ProfileReviewItemList: React.FC<Props> = ({ reviews }) => {
+  return (
+    <div>
+      {reviews.map((review) => (
+        <ProfileReviewItem key={review.reviewId} review={review} />
+      ))}
+    </div>
+  );
 };
 
 export default ProfileReviewItemList;
