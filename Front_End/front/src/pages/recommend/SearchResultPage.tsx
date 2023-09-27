88fa417dde5d0f49d2e3d830f97b4c208deda788
이수연 <dltd0912@gmail.com>
2023-09-27 10:18:13 +0900

    Feat: 검색 기능 추가

--- Front_End/front/src/pages/recommend/SearchResultPage.tsx
+++ Front_End/front/src/pages/recommend/SearchResultPage.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import ResultContainers from "../../components/search/ResultContainers";
 import SearchContainer from "../../components/search/SearchContainer";
+import { MainPaddingContainer } from "../../commons/style/layoutStyle";
+import { MovieType } from "../../components/movies/MovieItemList";
 
 const SearchResultPage = () => {
+  const [searchResults, setSearchResults] = useState<MovieType[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
   return (
-    <>
-      <SearchContainer />
-      <ResultContainers />
-    </>
+    <MainPaddingContainer>
+      <div>
+        <SearchContainer
+          setSearchResults={setSearchResults}
+          setSearchTerm={setSearchTerm}
+        />
+        <ResultContainers
+          searchResults={searchResults}
+          searchTerm={searchTerm}
+        />
+      </div>
+    </MainPaddingContainer>
   );
 };
 
