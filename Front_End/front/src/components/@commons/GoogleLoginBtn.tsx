03cb333cc5e9f4f374dee95dfba54c4a885ab878
함소명 <gkathaud@naver.com>
2023-09-27 15:52:03 +0900

    S09P22B204-143 Feat: 유튜브 구독 리스트 요청

--- Front_End/front/src/components/@commons/GoogleLoginBtn.tsx
+++ Front_End/front/src/components/@commons/GoogleLoginBtn.tsx
@@ -1,90 +1,36 @@
-import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { useSetRecoilState } from "recoil";
-// import { UserInfoState, LoginState } from "../../store/State";
-import qs from "qs";
-import Btn from "../atoms/Btn";
 import styled from "styled-components";
+
+import Btn from "../atoms/Btn";
 import { FlexCenter } from "../../commons/style/SharedStyle";
 import { GOOGLE_LOGIN } from "../../commons/constants/String";
-import axios from "axios";
-
-const CLIENT_ID: string = "781680119308-d0jbnhpcmrcj7fb65ls9crj7lh6k7v9q.apps.googleusercontent.com";
-const AUTHORIZE_URI = "https://accounts.google.com/o/oauth2/v2/auth";
-
-const queryStr = qs.stringify({
-  client_id: CLIENT_ID,
-  // redirect_uri: window.location.href,
-  redirect_uri: "http://localhost:3000/login/oauth2/code/google",
-  response_type: "token",
-  scope: "https://www.googleapis.com/auth/contacts.readonly",
-});
-
-const loginUrl = AUTHORIZE_URI + "?" + queryStr;
 
 const GoogleLoginBtn = () => {
-  const { access_token } = qs.parse(window.location.hash.substr(1));
-
-  if (!access_token) {
-    window.location.assign(loginUrl);
-    return null;
-  }
-  console.log("로그: "+access_token)
-
   return (
     <>
-      <GoogleOAuthProvider clientId={CLIENT_ID}>
-        <GoogleInnerComponent />
-      </GoogleOAuthProvider>
+      <GoogleInnerComponent />
     </>
   );
 };
 
+const scopes = [
+  'https://www.googleapis.com/auth/youtube',
+  'https://www.googleapis.com/auth/youtube.force-ssl',
+  'https://www.googleapis.com/auth/youtube.readonly',
+  'https://www.googleapis.com/auth/youtubepartner'
+];
+
+const scopeString = scopes.join(' ');
+
 const GoogleInnerComponent = () => {
-  // const setUserInfo = useSetRecoilState();
-  // const setLogin = useSetRecoilState(LoginState);
-
-  const googleSocialLogin = useGoogleLogin({
-    onSuccess: async (res) => {
-      console.log(res)
-      const receivedAccessToken = res.access_token;
-      console.log("accessToken: "+receivedAccessToken)
-
-      //axios 헤더 설정
-      const instance = axios.create({
-        baseURL: 'https://www.googleapis.com/youtube/v3/', 
-        headers: {
-          Authorization: `Bearer ${receivedAccessToken}`,
-        },
-      });
-
-      //구독 정보 가져오기.
-      instance.get('subscriptions', {
-        params: {
-          part: 'snippet',
-          mine: true,
-          maxResults: 50,
-        },
-      })
-      .then((response) => {
-        // 요청이 성공한 경우, response에서 데이터를 처리합니다.
-        console.log('YouTube API 응답 데이터:', response.data);
-      })
-      .catch((error) => {
-        // 요청이 실패한 경우, 에러를 처리합니다.
-        console.error('YouTube API 요청 실패:', error);
-      });
-
-    },
-    onError: () => {
-      console.error("로그인 에러임");
-    },
-  });
+  const handleGoogleLogin = () => {
+    window.location.href =
+      `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=http://localhost:3000/addinfo&response_type=code&client_id=781680119308-d0jbnhpcmrcj7fb65ls9crj7lh6k7v9q.apps.googleusercontent.com&scope=${scopeString}&access_type=offline`;
+  };
 
   return (
     <>
-      <StyledLoginBtn color="White" size="Medium" onClick={googleSocialLogin}>
-        <StyledGoogleLogo src="assets/Logo/GoogleLogo.svg" alt="GoogleLogo" />
+      <StyledLoginBtn color="White" size="Medium" onClick={handleGoogleLogin}>
+        <StyledGoogleLogo src="/assets/Logo/GoogleLogo.svg" alt="GoogleLogo" />
         {GOOGLE_LOGIN}
       </StyledLoginBtn>
     </>
