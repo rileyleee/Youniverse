8b466bb632338bca74d9ad98e536f4e01bd1170f
이은경 <whddlto2782@gmail.com>
2023-09-29 02:22:53 +0900

    Feat: 프로필 구성

--- Front_End/front/src/components/organisms/OtherProfileContainer.tsx
+++ Front_End/front/src/components/organisms/OtherProfileContainer.tsx
@@ -1,11 +1,32 @@
+import { useState, useEffect } from "react";
+import { useRecoilValue } from "recoil";
 import styled from "styled-components";
+import { UserDetailInfoState } from "../../pages/store/State";
 import { SEARCH_USER_PAGE } from "../../commons/constants/String";
 import { FlexCenter } from "../../commons/style/SharedStyle";
 import Wrapper from "../atoms/Wrapper";
 import Text from "../atoms/Text";
 import ProfileReview from "../review/ProfileReview";
+import UserZodiacSign from "../users/UserZodiacSign";
+import SoulMovieItemList from "../movies/SoulMovieItemList";
+import { UserType } from "../../pages/profile/MyProfilePage";
+import { getMember } from "../../apis/FrontendApi";
 
 const OtherProfileContainer = () => {
+  const { memberId } = useRecoilValue(UserDetailInfoState);
+  const [memberData, setMemberData] = useState<UserType | null>(null);
+
+  useEffect(() => {
+    getMember(Number(memberId))
+      .then((response) => {
+        console.log("개별 회원 조회", `${memberId}번 회원=`, response.data);
+        setMemberData(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [memberId]);
+
   return (
     <StyledStandardWhiteGhostWrapper
       size="Standard"
@@ -14,8 +35,10 @@ const OtherProfileContainer = () => {
     >
       <Text size="X-Large" color="Black" fontFamily="PyeongChang-Light">
         {SEARCH_USER_PAGE}
-        <ProfileReview />
       </Text>
+      <UserZodiacSign />
+      <SoulMovieItemList />
+      <ProfileReview />
     </StyledStandardWhiteGhostWrapper>
   );
 };
