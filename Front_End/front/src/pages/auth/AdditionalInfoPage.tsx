17f101507c56aed09dc5286901db3fc11ba80ae6
samsung <gkathaud@naver.com>
2023-09-28 20:41:43 +0900

    S09P22B204-143 Feat: 내 계정 정보 가져오기

--- Front_End/front/src/pages/auth/AdditionalInfoPage.tsx
+++ Front_End/front/src/pages/auth/AdditionalInfoPage.tsx
@@ -34,6 +34,7 @@ const AdditionalInfoPage = () => {
       requestData.append('client_secret', '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');
       const apiKey = 'AIzaSyAbqPlaSAh5ppO1pOrnOS21vx0mIMGmMfs HTTP/1.1';
 
+
       // 토큰을 얻는 POST 요청
       axios.post(tokenUrl, requestData, {
         headers: {
@@ -45,112 +46,139 @@ const AdditionalInfoPage = () => {
         console.log('토큰: ', access_token);
 
         setAccessToken(access_token); // 토큰을 상태 변수에 저장
+
+        const apiHeaders = {
+          'Authorization': 'Bearer ' + access_token,
+          'Accept': 'application/json'
+        };
+        
       
-        /** 구독 리스트 요청 */
-        axios.get('https://youtube.googleapis.com/youtube/v3/subscriptions', {
+        // /** 구독 리스트 불러와 구독 채널의 영상 데이터 가져오기 */
+        // // 구독 리스트 요청 
+        // axios.get('https://youtube.googleapis.com/youtube/v3/subscriptions', {
+        //   params: {
+        //     maxResults: 50, // maxResults 파라미터 추가
+        //     part: 'snippet', // 필요한 파트 추가
+        //     mine: true,
+        //     key: apiKey
+        //   },  
+        //   headers: apiHeaders,
+        // })
+        // .then((response) => {
+        //   // console.log('구독 응답 데이터:', response.data.items);
+        //   interface SubscriptionItem {
+        //     etag: string;
+        //     id: string;
+        //     kind: string;
+        //     snippet: {
+        //       channelId: string;
+        //       description: string;
+        //       publishedAt: string;
+        //       resourceId: {
+        //         kind: string;
+        //         channelId: string;
+        //       };
+        //       title: string;
+        //     };
+        //   }
+          
+        //   const data: SubscriptionItem[] = response.data.items; // 데이터 배열을 직접 설정
+        //   const channelId: string[] = data.map(item => item.snippet.resourceId.channelId);//채널 아이디
+        //   const titles: string[] = data.map(item => item.snippet.title);//채널 제목
+        //   const description: string[] = data.map(item => item.snippet.description);//채널 설명
+        //   console.log("데이터 추출: ", titles, channelId, description);
+
+        //   //채널 안 영상 정보 가져오기
+        //   const sendAxiosRequests = async (channelIds: string[]) => {
+        //     for (const channelId of channelIds) {
+        //       try {
+        //         axios.get('https://youtube.googleapis.com/youtube/v3/search', {
+        //           params: {
+        //             type: 'video',
+        //             maxResults: 50,
+        //             part: 'snippet',
+        //             channelId: channelId, // 각 채널 ID에 대한 요청을 보냅니다.
+        //             key: apiKey 
+        //           },  
+        //           headers: apiHeaders,
+        //         }).then((response) => {
+        //           // console.log('영상 정보: ', response.data.items)
+
+        //           interface VideoItem      {
+        //             "kind": "youtube#searchResult",
+        //             "etag": string,
+        //             "id": {
+        //               "kind": string,
+        //               "videoId": string
+        //             },
+        //             "snippet": {
+        //               "channelId": string,
+        //               "title": string,
+        //               "description": string,
+        //               "channelTitle": string
+        //             };
+        //           }
+
+        //           const data: VideoItem[] = response.data.items;
+        //           const videoId: string[] = data.map(item => item.id.videoId);
+        //           const titles: string[] = data.map(item => item.snippet.title);
+        //           const description: string[] = data.map(item => item.snippet.description);
+
+        //           // 각 인덱스 위치의 항목을 합쳐서 하나의 배열로 만듭니다.
+        //           const combinedData: Array<{ videoId: string, title: string, description: string }> = videoId.map((_, index) => ({
+        //             videoId: videoId[index],
+        //             title: titles[index],
+        //             description: description[index]
+        //           }));
+        //           console.log("채널 영상 추출: ", combinedData);
+
+        //         })
+        //         .catch((error) => {
+        //           console.error('영상 정보 요청 실패:', error);
+        //         });
+
+        //       } catch (error) {
+        //         console.error(`Error fetching data for channel ${channelId}:`, error);
+        //       }
+        //     }
+
+        //   };
+        //   sendAxiosRequests(channelId);
+
+        // })
+        // .catch((error) => {
+        //   console.error('구독 정보 요청 실패:', error);
+        // });
+        
+        /** 내 계정 채널 재생목록, 재생목록의 영상 데이터 가져오기 */
+        // 재생목록 요청 
+        axios.get('https://youtube.googleapis.com/youtube/v3/channels', {
           params: {
             maxResults: 50, // maxResults 파라미터 추가
             part: 'snippet', // 필요한 파트 추가
             mine: true,
             key: apiKey
           },  
-          headers: {
-            'Authorization': 'Bearer '+access_token,
-            'Accept': 'application/json'
-          },
+          headers: apiHeaders,
         })
-        .then((response) => {
-          // console.log('구독 응답 데이터:', response.data.items);
-          interface SubscriptionItem {
-            etag: string;
-            id: string;
-            kind: string;
-            snippet: {
-              channelId: string;
-              description: string;
-              publishedAt: string;
-              resourceId: {
-                kind: string;
-                channelId: string;
-              };
-              title: string;
-            };
-          }
-          
-          const data: SubscriptionItem[] = response.data.items; // 데이터 배열을 직접 설정
-          const channelId: string[] = data.map(item => item.snippet.resourceId.channelId);//채널 아이디
-          const titles: string[] = data.map(item => item.snippet.title);//채널 제목
-          const description: string[] = data.map(item => item.snippet.description);//채널 설명
-          console.log("데이터 추출: ", titles, channelId, description);
+          .then((response) => {
+          const MyData = response.data.items[0];
+          const Nikname = MyData.snippet.title;
+          const description = MyData.snippet.description;
+          const channelId = MyData.id;
+          console.log("내 계정 데이터: ", Nikname, description, channelId);
 
-          //채널 안 영상 정보 가져오기
-          const sendAxiosRequests = async (channelIds: string[]) => {
-            for (const channelId of channelIds) {
-              try {
-                axios.get('https://youtube.googleapis.com/youtube/v3/search', {
-                  params: {
-                    type: 'video',
-                    maxResults: 50,
-                    part: 'snippet',
-                    channelId: channelId, // 각 채널 ID에 대한 요청을 보냅니다.
-                    key: apiKey 
-                  },  
-                  headers: {
-                    'Authorization': 'Bearer '+access_token,
-                    'Accept': 'application/json'
-                  },
-                }).then((response) => {
-                  // console.log('영상 정보: ', response.data.items)
 
-                  interface VideoItem      {
-                    "kind": "youtube#searchResult",
-                    "etag": string,
-                    "id": {
-                      "kind": string,
-                      "videoId": string
-                    },
-                    "snippet": {
-                      "channelId": string,
-                      "title": string,
-                      "description": string,
-                      "channelTitle": string
-                    };
-                  }
 
-                  const data: VideoItem[] = response.data.items;
-                  const videoId: string[] = data.map(item => item.id.videoId);
-                  const titles: string[] = data.map(item => item.snippet.title);
-                  const description: string[] = data.map(item => item.snippet.description);
-
-                  // 각 인덱스 위치의 항목을 합쳐서 하나의 배열로 만듭니다.
-                  const combinedData: Array<{ videoId: string, title: string, description: string }> = videoId.map((_, index) => ({
-                    videoId: videoId[index],
-                    title: titles[index],
-                    description: description[index]
-                  }));
-
-                  console.log("채널 영상 추출: ", combinedData);
-
-                })
-                .catch((error) => {
-                  console.error('영상 정보 요청 실패:', error);
-                });
-                
-              } catch (error) {
-                console.error(`Error fetching data for channel ${channelId}:`, error);
-              }
-            }
-          };
-          sendAxiosRequests(channelId);
-
+            
         })
         .catch((error) => {
-          console.error('구독 정보 요청 실패:', error);
+          console.error('내 계정 정보 요청 실패:', error);
         });
 
       })
       .catch((error) => {
-        console.error('요청 실패:', error);
+        console.error('토큰 요청 실패:', error);
       });
     }
   }, [codes]);
