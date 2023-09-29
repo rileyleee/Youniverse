daf3db4bba98f87f79735161ca43b1d94f19cd83
이은경 <whddlto2782@gmail.com>
2023-09-30 00:15:40 +0900

    Feat: 프로필 컴포넌트 구성 작업

--- Front_End/front/src/pages/profile/OtherUserProfilePage.tsx
+++ Front_End/front/src/pages/profile/OtherUserProfilePage.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 import OtherProfileContainer from "../../components/organisms/OtherProfileContainer";
 import { MainPaddingContainer } from "../../commons/style/layoutStyle";
@@ -7,12 +9,28 @@ import {
   FlexRowBetween,
   FlexColBetween,
 } from "../../commons/style/SharedStyle";
+import { UserDetailInfoState } from "../../pages/store/State";
+import { getMember } from "../../apis/FrontendApi";
+import { UserType } from "../../pages/profile/MyProfilePage";
 
 const OtherUserProfilePage = () => {
+  const { memberId } = useRecoilValue(UserDetailInfoState);
+  const [memberData, setMemberData] = useState<UserType | null>(null);
+  useEffect(() => {
+    getMember(Number(memberId))
+      .then((response) => {
+        console.log("개별 회원 조회", `${memberId}번 회원=`, response.data);
+        setMemberData(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [memberId]);
   return (
     <MainPaddingContainer>
       <StyledOtherUserProfile>
-        <OtherProfileContainer />
+        <OtherProfileContainer 
+        memberData = {memberData}/>
         <StyledSearchRecommend>
           <UserSearchContainer />
           <UserRecommendContainer />
