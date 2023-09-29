bef9828d04952d472f9cdfd65ebcf9d6d95d605e
samsung <gkathaud@naver.com>
2023-09-29 19:17:04 +0900

    S09P22B204-227 Feat: 데이터 전처리 추가

--- Fast_Api/youniverse/routers/youtube.py
+++ Fast_Api/youniverse/routers/youtube.py
@@ -1,20 +1,45 @@
 from fastapi import APIRouter
 from youniverse.repository import testRepository
 from youniverse.schemas import youtube
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+# NLTK에서 불용어 다운로드
+nltk.download('stopwords')
+nltk.download('punkt')
 
 router = APIRouter(
     prefix="/youtube",
     tags=["youtube"]
 )
 
-@router.get("/")
-async def get_Test():
-    received_data ="이 채널에서는 IT 업계에서의 앱 개발자는 어떻게 하루를 보낼까 궁금해 하시는 분들을 위해서 만들어졌습니다.또한 저의 일상도 녹여져있으나, 개발자들 간의 소통도 취준생 및 채널을 방문해주시는 모든 분들 환영 합니다"
-    return received_data
+
 
 @router.post("/")
-async def first_get(data_request: youtube):
+async def data_get(data_request: youtube):
     received_data = data_request.data
 
     print("Received data:", received_data)
     return
+
+
+# 전처리 함수 정의
+def preprocess(text):
+    # 한글, 영문, 숫자를 제외한 모든 문자 제거
+    text = re.sub(r'[^가-힣a-zA-Z0-9\s]', '', text)
+
+    #불용어 처리
+    # # 1. 불용어 라이브러리 nltk을 사용하고자 하였지만 제대로 처리되지 않아 불용어를 직접 넣어줬다.
+    # stop_words = set(stopwords.words('english'))
+    # words = word_tokenize(text)
+    # words = [word for word in words if word.lower() not in stop_words]
+
+    # 2. 불용어 제거, 필요에 따라 추가 가능
+    stopwords = ['이', '는', '에서', '의', '을', '어떻게', '하시는', '분들', '위해서', '만들어졌습니다', '또한', '저의', '녹여져있으나', '들', '간의', '및', '을', '해주시는', '모든', '합니다', '위해', '에서의', '에서는']
+    words = okt.morphs(text)
+    words = [word for word in words if word not in stopwords]
+
+    return ' '.join(words)
