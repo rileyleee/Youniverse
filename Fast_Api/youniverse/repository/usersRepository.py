aeb75fd1d59e226ee9150aeb489d1e6b20a61bca
samsung <gkathaud@naver.com>
2023-10-01 20:34:20 +0900

    S09P22B204-227 Feat: 알고리즘 분류

---
+++ Fast_Api/youniverse/repository/usersRepository.py
@@ -0,0 +1,18 @@
+from youniverse import models
+from youniverse.database import engineconn
+
+engine = engineconn()
+session = engine.sessionmaker()
+
+# 모든 회원의 키워드 가져오기 (내 이메일 제외)
+def get_member_keyword(myEmail):
+    all_members = session.query(models.Member.member_id, models.Keyword.keyword_name).join(models.KeywordMember, models.Keyword).filter(
+        models.Member.email != myEmail).all()
+    member_keywords = {}
+
+    for member_id, keyword_name in all_members:
+        if member_id not in member_keywords:
+            member_keywords[member_id] = []
+        member_keywords[member_id].append(keyword_name)
+
+    return member_keywords
