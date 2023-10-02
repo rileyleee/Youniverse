5988d31860602eec5cd53ec9a2702883ff3f8a5d
선다영 <seondy99@gmail.com>
2023-10-03 05:01:55 +0900

    Feat: 인생영화등록 및 렌더링 적용

--- Front_End/front/src/components/search/ResultContainers.tsx
+++ Front_End/front/src/components/search/ResultContainers.tsx
@@ -5,18 +5,23 @@ import SearchMovieItemList from "../movies/SearchMovieItemList";
 import { MovieType } from "../../types/MovieType";
 
 interface ResultContainersProps {
+  onMovieSelect?: (movie: MovieType) => void;
   searchResults: MovieType[];
   searchTerm: string;
 }
 
-const ResultContainers: React.FC<ResultContainersProps> = ({ searchResults, searchTerm }) => {
+const ResultContainers: React.FC<ResultContainersProps> = ({
+  onMovieSelect,
+  searchResults,
+  searchTerm,
+}) => {
   return (
     <StyledWrapper size="Standard" color="WhiteGhost" padding="Medium">
       <Text size="Medium" color="Black" fontFamily="YESGothic-Bold">
         #{searchTerm}
       </Text>
       {searchResults.length > 0 ? (
-        <SearchMovieItemList movies={searchResults} />
+        <SearchMovieItemList onMovieSelect={onMovieSelect} movies={searchResults} />
       ) : (
         <Text size="Medium" color="Black" fontFamily="YESGothic-Bold">
           검색결과가 없습니다
@@ -29,6 +34,6 @@ const ResultContainers: React.FC<ResultContainersProps> = ({ searchResults, sear
 export default ResultContainers;
 
 const StyledWrapper = styled(Wrapper)`
-  max-height: 600px;  // 원하는 높이 설정
+  max-height: 600px; // 원하는 높이 설정
   overflow-y: auto;
 `;
