40539ec2f16dd1fdabf9dbd6eada073bb3ba9e04
이수연 <dltd0912@gmail.com>
2023-10-02 22:55:28 +0900

    Modify: movieItemList 수정

--- Front_End/front/src/components/movies/MovieItemList.tsx
+++ Front_End/front/src/components/movies/MovieItemList.tsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 import {
   UserDetailInfoState,
@@ -14,43 +12,25 @@ import { FlexRowBetween } from "../../commons/style/SharedStyle";
 import Btn from "../atoms/Btn";
 import Text from "../atoms/Text";
 import MovieItem from "./MovieItem";
-import { MovieType, OTTType } from "../../types/MovieType";
 import { getAllMovies } from "../../apis/FrontendApi";
+import { MovieType, OTTType } from "../../types/MovieType";
 
 type Props = {
   filterOTT?: string | null;
   listType?: string | null;
   movies?: MovieType[];
-  showMoreButton?: boolean; // 더보기 버튼 더보기 페이지에는 안보여야 하니까
+  showMoreButton?: boolean;
   useSlider?: boolean;
-};
-
-const convertOTTNameToId = (
-  ottName: string | null | undefined
-): number | null => {
-  if (!ottName) return null;
-  const ottList = [
-    { name: "넷플릭스", id: 8 },
-    { name: "디즈니플러스", id: 337 },
-    { name: "왓챠", id: 97 },
-    { name: "애플티비", id: 2 },
-    { name: "애플티비플러스", id: 350 },
-    { name: "웨이브", id: 356 },
-  ];
-
-  const ott = ottList.find((o) => o.name === ottName);
-  return ott ? ott.id : null;
 };
 
 const sliderSettings = {
   infinite: true,
-  slidesToShow: 8, // 한 번에 보여줄 아이템 수
+  slidesToShow: 8,
   swipeToSlide: true,
-  autoplay: true, // 자동 캐러셀
+  autoplay: true,
   autoplaySpeed: 3000,
-  arrows: true, // 좌,우 버튼
-  pauseOnHover: true, // hover시 정지
-  // 다른 설정들도 추가 가능
+  arrows: true,
+  pauseOnHover: true,
 };
 
 const MovieItemList: React.FC<Props> = ({
@@ -66,61 +46,52 @@ const MovieItemList: React.FC<Props> = ({
   const memberAge = useRecoilValue(UserJoinInfoState).age;
   const memberGender = useRecoilValue(UserJoinInfoState).gender;
 
+  const sortTypeMap = {
+    // 함수 외부에서 sortTypeMap 정의
+    "선호도기반 추천 영화": 1,
+    [`${memberAge}세 ${memberGender} 추천 영화`]: 2,
+    "유튜브 기반 추천 영화": 3,
+  };
+
+  // 더보기 버튼 클릭 처리
   const handleMoreClick = () => {
-    let sortType: number | null = null;
+    const sortType = sortTypeMap[listType || ""]; // listType이 null일 경우 빈 문자열로 처리
 
-    switch (listType) {
-      case "선호도기반 추천 영화":
-        sortType = 1;
-        break;
-      case `${memberAge}세 ${memberGender} 추천 영화`:
-        sortType = 2;
-        break;
-      case "유튜브 기반 추천 영화":
-        sortType = 3;
-        break;
-      default:
-        break;
-    }
-
-    // navigate to the recommendation page with a sort type
     if (sortType) {
       navigate(`/recommend/more?sort=${sortType}`);
     }
   };
+  
+  const convertOTTNameToId = (
+    ottName: string | null | undefined
+  ): number | null => {
+    if (!ottName) return null;
+    const ottList = [
+      { name: "넷플릭스", id: 8 },
+      { name: "디즈니플러스", id: 337 },
+      { name: "왓챠", id: 97 },
+      { name: "애플티비", id: 2 },
+      { name: "애플티비플러스", id: 350 },
+      { name: "웨이브", id: 356 },
+    ];
 
+    const ott = ottList.find((o) => o.name === ottName);
+    return ott ? ott.id : null;
+  };
+
+  // 영화 데이터 가져오기
   useEffect(() => {
-    let requestParams: any = { page: 0, size: 20 };
-
-    if (listType === "선호도기반 추천 영화" || listType === "1") {
-      requestParams = {
-        ...requestParams,
-        "member-id": memberId,
-        type: 1,
-      };
-    } else if (
-      listType === `${memberAge}세 ${memberGender} 추천 영화` ||
-      listType === "2"
-    ) {
-      requestParams = {
-        ...requestParams,
-        "member-id": memberId,
-        type: 2,
-      };
-    } else if (listType === "유튜브 기반 추천 영화" || listType === "3") {
-      requestParams = {
-        ...requestParams,
-        "member-id": memberId,
-        type: 3,
-      };
-    }
+    const requestParams: any = {
+      page: 0,
+      size: 20,
+      "member-id": memberId,
+      type: sortTypeMap[listType || ""] || null, // listType이 null일 경우 빈 문자열로 처리
+    };
 
     getAllMovies(requestParams)
       .then((response) => {
-        console.log("API Response:", response);
-
+        // TODO: convertOTTNameToId 함수를 정의하거나 가져와야 함
         const targetOttId = convertOTTNameToId(filterOTT);
-
         const filteredMovies = response.data.content.filter(
           (movie: MovieType) => {
             if (!targetOttId) return true;
@@ -130,7 +101,6 @@ const MovieItemList: React.FC<Props> = ({
           }
         );
 
-        console.log("Filtered Movies:", filteredMovies);
         setMovies(filteredMovies);
       })
       .catch((err) => {
@@ -141,7 +111,6 @@ const MovieItemList: React.FC<Props> = ({
 
   return (
     <>
-      {/* {filterOTT} */}
       <StyledListBtn>
         <Text size="Large" color="White" fontFamily="PyeongChang-Bold">
           {listType}
