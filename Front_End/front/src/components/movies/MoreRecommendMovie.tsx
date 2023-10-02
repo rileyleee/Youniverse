09269c4f9ae2ab08819b90cf554a2aaa3d9d4320
이수연 <dltd0912@gmail.com>
2023-10-03 02:17:53 +0900

    Modify: 더보기 페이지 ott 클릭 시 영화 계속 쌓이는거 수정

--- Front_End/front/src/components/movies/MoreRecommendMovie.tsx
+++ Front_End/front/src/components/movies/MoreRecommendMovie.tsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Wrapper from "../atoms/Wrapper";
 import MovieItemList from "./MovieItemList";
 
 type MovieProps = {
   selectedOTT: string | null;
-  listType?: string | null; 
+  listType?: string | null;
 };
 
-const MoreRecommendMovie: React.FC<MovieProps> = ({ selectedOTT, listType }) => {
+const MoreRecommendMovie: React.FC<MovieProps> = ({
+  selectedOTT,
+  listType,
+}) => {
+  const [page, setPage] = useState(0);
+
+  const handleScroll = (e: any) => {
+    const target = e.target;
+    if (target.scrollHeight - target.scrollTop === target.clientHeight) {
+      setPage((prev) => prev + 1); // 페이지 번호 증가
+    }
+  };
+
+  useEffect(() => {
+    setPage(0); // selectedOTT가 변경될 때 페이지 번호를 리셋
+  }, [selectedOTT]);
+
   return (
-    <Wrapper size="Standard" color="WhiteGhost" padding="Medium">
-      <MovieItemList filterOTT={selectedOTT} listType={listType} useSlider={false} />
+    <Wrapper
+      size="Standard"
+      color="WhiteGhost"
+      padding="Medium"
+      onScroll={handleScroll}
+    >
+      <MovieItemList
+        filterOTT={selectedOTT}
+        listType={listType}
+        useSlider={false}
+        page={page}
+      />
     </Wrapper>
   );
 };
-
 
 export default MoreRecommendMovie;
