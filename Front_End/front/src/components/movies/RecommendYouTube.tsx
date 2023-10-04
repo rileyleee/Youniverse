30c3e6fb20b944e3593412aa6f3c1802c62ed845
이은경 <whddlto2782@gmail.com>
2023-10-04 22:38:52 +0900

    Feat: 나의 행성 컴포넌트 반영

--- Front_End/front/src/components/movies/RecommendYouTube.tsx
+++ Front_End/front/src/components/movies/RecommendYouTube.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 import {
   RECOMMEND_PAGE_CONTAINER_KEYWORD,
@@ -11,20 +11,40 @@ import Wrapper from "../atoms/Wrapper";
 import Text from "../atoms/Text";
 import LineChartWrapper from "../chart/LineChartWrapper";
 import styled from "styled-components";
+import MyOTTPlanet from "../users/MyOTTPlanet";
+import { useRecoilValue } from "recoil";
+import { UserDetailInfoState } from "../../pages/store/State";
+import { getMember } from "../../apis/FrontendApi";
+import { UserType } from "../../pages/profile/MyProfilePage";
 
 const RecommendYouTube = () => {
+  const currentId = useRecoilValue(UserDetailInfoState).memberId;
+  const [memberData, setMemberData] = useState<UserType | null>(null);
+
+  useEffect(() => {
+    getMember(Number(currentId))
+      .then((response) => {
+        console.log("개별 회원 조회", `${currentId}번 회원=`, response.data);
+        setMemberData(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [currentId]);
+
   return (
     <StyledWrapper size="YouTube" color="WhiteGhost" padding="Narrow">
-        <StyledYouTubeStar>
-          <Text size="Medium" color="White" fontFamily="PyeongChang-Light">
-            {RECOMMEND_PAGE_CONTAINER_KEYWORD}
-          </Text>
-          <LineChartWrapper chartWidth="100%" chartHeight="100%" />
-        </StyledYouTubeStar>
+      <StyledYouTubeStar>
+        <Text size="Medium" color="White" fontFamily="PyeongChang-Light">
+          {RECOMMEND_PAGE_CONTAINER_KEYWORD}
+        </Text>
+        <LineChartWrapper chartWidth="100%" chartHeight="100%" />
+      </StyledYouTubeStar>
       <StyledYouTubeRec>
         <Text size="Medium" color="White" fontFamily="PyeongChang-Light">
           {RECOMMEND_PAGE_CONTAINER_YOUTUBE}
         </Text>
+        <MyOTTPlanet memberData={memberData} />
       </StyledYouTubeRec>
     </StyledWrapper>
   );
