4eefdd32870a4cd5af03275eccaae5d908863e8b
함소명 <gkathaud@naver.com>
2023-10-04 10:54:58 +0900

    Feat: fastapi 배포 테스트2

--- Fast_Api/youniverse/routers/users.py
+++ Fast_Api/youniverse/routers/users.py
@@ -14,7 +14,8 @@ async def get_Test():
     result_users = user.similarily(member_id)
 
     # 결과 값을 바탕으로 회원정보 리턴
-    return user.get_members_info(result_users)
+    # return user.get_members_info(result_users)
+    return "배포 테스트"
 
 @router.get("/")
 async def get_Users(member_id: int):
