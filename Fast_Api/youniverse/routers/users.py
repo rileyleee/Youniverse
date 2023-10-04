85ec81c062e878250bb1af9b1f1634087406cce6
함소명 <gkathaud@naver.com>
2023-10-04 10:02:46 +0900

    Modify: 사용자 추천 Json 수정

--- Fast_Api/youniverse/routers/users.py
+++ Fast_Api/youniverse/routers/users.py
@@ -21,6 +21,9 @@ async def get_Users(member_id: int):
     # 사용자 필터링
     result_users = user.similarily(member_id)
 
-    # 결과 값을 바탕으로 회원정보 리턴
-    return user.get_members_info(result_users)
+    # 회원정보
+    users_info = user.get_members_info(result_users)
+
+    # JSON 응답 반환
+    return {"users": users_info}
 
