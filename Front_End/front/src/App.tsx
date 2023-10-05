f92d17789d6ca18f348b46c8f0c8c1105ced70aa
선다영 <seondy99@gmail.com>
2023-10-05 17:02:43 +0900

    Design: 메인페이지 비로그인 디자인 변경

--- Front_End/front/src/App.tsx
+++ Front_End/front/src/App.tsx
@@ -1,6 +1,11 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode } from "react";
 import { RecoilRoot } from "recoil";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 // import StarryBackground from "./commons/style/StarryBackground";
 import { GlobalStyles } from "./commons/style/GlobalStyle";
 import { ROUTES } from "./commons/constants/Routes";
@@ -42,78 +47,99 @@ function ProtectedRoute({ isAuthenticated, element }: ProtectedRouteProps) {
   return <>{element}</>;
 }
 
-
 function ProtectedApp() {
-
   const { isAuthenticated, isChecking } = useAuth();
 
   return (
-      <Router>
-        <GlobalStyles />
-        <Header />
-        {isChecking ? (
-          <div></div>
-        ) : (
+    <Router>
+      {/* <StarryBackground /> */}
+      <GlobalStyles />
+      <Header />
+      {isChecking ? (
+        <div></div>
+      ) : (
         <>
-        <Routes>
-          <Route path={ROUTES.MAIN} Component={MAIN} />
-          <Route path={ROUTES.LOADING} Component={LOADING} />
-          <Route path={ROUTES.ADDINFO} Component={ADDINFO} />
-          <Route 
-            path={ROUTES.SURVEY}
-            element={
-              <ProtectedRoute isAuthenticated={isAuthenticated} element={<SURVEY />} />
-            }
-          />
-          <Route 
-            path={ROUTES.OTTSELECT}
-            element={
-              <ProtectedRoute isAuthenticated={isAuthenticated} element={<OTTSELECT />} />
-            }
-          />
-          <Route 
-            path={ROUTES.MYPAGE}
-            element={
-              <ProtectedRoute isAuthenticated={isAuthenticated} element={<MYPAGE />} />
-            }
-          />
-          <Route 
-            path={ROUTES.PROFILE}
-            element={
-              <ProtectedRoute isAuthenticated={isAuthenticated} element={<PROFILE />} />
-            }
-          />
-          <Route 
-            path={ROUTES.SEARCH}
-            element={
-              <ProtectedRoute isAuthenticated={isAuthenticated} element={<SEARCH />} />
-            }
-          />
-          <Route 
-            path={ROUTES.RECOMMEND}
-            element={
-              <ProtectedRoute isAuthenticated={isAuthenticated} element={<RECOMMEND />} />
-            }
-          />
-          <Route 
-            path={ROUTES.MOVIE_DETAIL}
-            element={
-              <ProtectedRoute isAuthenticated={isAuthenticated} element={<MOVIE_DETAIL />} />
-            }
-          />
-          <Route 
-            path={ROUTES.RECOMMEND_MORE}
-            element={
-              <ProtectedRoute isAuthenticated={isAuthenticated} element={<RECOMMEND_MORE />} />
-            }
-          />
-          <Route path={ROUTES.NOTFOUND} Component={NOTFOUND} />
-        </Routes>
+          <Routes>
+            <Route path={ROUTES.MAIN} Component={MAIN} />
+            <Route path={ROUTES.LOADING} Component={LOADING} />
+            <Route path={ROUTES.ADDINFO} Component={ADDINFO} />
+            <Route
+              path={ROUTES.SURVEY}
+              element={
+                <ProtectedRoute
+                  isAuthenticated={isAuthenticated}
+                  element={<SURVEY />}
+                />
+              }
+            />
+            <Route
+              path={ROUTES.OTTSELECT}
+              element={
+                <ProtectedRoute
+                  isAuthenticated={isAuthenticated}
+                  element={<OTTSELECT />}
+                />
+              }
+            />
+            <Route
+              path={ROUTES.MYPAGE}
+              element={
+                <ProtectedRoute
+                  isAuthenticated={isAuthenticated}
+                  element={<MYPAGE />}
+                />
+              }
+            />
+            <Route
+              path={ROUTES.PROFILE}
+              element={
+                <ProtectedRoute
+                  isAuthenticated={isAuthenticated}
+                  element={<PROFILE />}
+                />
+              }
+            />
+            <Route
+              path={ROUTES.SEARCH}
+              element={
+                <ProtectedRoute
+                  isAuthenticated={isAuthenticated}
+                  element={<SEARCH />}
+                />
+              }
+            />
+            <Route
+              path={ROUTES.RECOMMEND}
+              element={
+                <ProtectedRoute
+                  isAuthenticated={isAuthenticated}
+                  element={<RECOMMEND />}
+                />
+              }
+            />
+            <Route
+              path={ROUTES.MOVIE_DETAIL}
+              element={
+                <ProtectedRoute
+                  isAuthenticated={isAuthenticated}
+                  element={<MOVIE_DETAIL />}
+                />
+              }
+            />
+            <Route
+              path={ROUTES.RECOMMEND_MORE}
+              element={
+                <ProtectedRoute
+                  isAuthenticated={isAuthenticated}
+                  element={<RECOMMEND_MORE />}
+                />
+              }
+            />
+            <Route path={ROUTES.NOTFOUND} Component={NOTFOUND} />
+          </Routes>
         </>
-
-        )}
-        {/* <StarryBackground /> */}
-      </Router>
+      )}
+    </Router>
   );
 }
 
